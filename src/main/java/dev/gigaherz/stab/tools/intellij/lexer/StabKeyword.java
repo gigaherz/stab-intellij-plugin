a7be65db43e69bb9f1e3df579dd846748a04788a
gigaherz <gigaherz@gmail.com>
2023-07-27 18:15:47 +0200

    Small improvements.

--- src/main/java/dev/gigaherz/stab/tools/intellij/lexer/StabKeyword.java
+++ src/main/java/dev/gigaherz/stab/tools/intellij/lexer/StabKeyword.java
@@ -1,13 +1,28 @@
 package dev.gigaherz.stab.tools.intellij.lexer;
+/*
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
 
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
 import com.intellij.psi.tree.IElementType;
 import dev.gigaherz.stab.tools.intellij.StabLanguage;
 
-public class StabKeyword extends IElementType
+public class StabKeyword extends StabToken
 {
     private StabKeyword(String debugName)
     {
-        super(debugName, StabLanguage.INSTANCE);
+        super("KEYWORD_" + debugName);
     }
 
     public static final StabKeyword NONE = new StabKeyword("NONE");
